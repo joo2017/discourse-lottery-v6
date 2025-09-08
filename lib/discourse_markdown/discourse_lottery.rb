3a5367d0b04ac577d6632f148879bef39b1a82a4
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-09-08 13:11:52 +0900

    Update discourse_lottery.rb

--- lib/discourse_markdown/discourse_lottery.rb
+++ lib/discourse_markdown/discourse_lottery.rb
@@ -1,18 +1,28 @@
 # frozen_string_literal: true
 
-Discourse::Markdown.register_bbcode("lottery", "div") do |bbcode|
-  attrs =
-    [
-      "name",
-      "draw_at",
-      "winner_count",
-      "min_participants",
-      "status",
-      "backup_strategy",
-      "specified_winners",
-      "prize_image_url",
-      "notes"
-    ].map { |key| "data-#{key.dasherize}='#{bbcode.attrs[key]}'" if bbcode.attrs[key] }.compact.join(" ")
+# 路径: discourse-lottery-v6/lib/discourse_markdown/discourse_lottery.rb
 
-  "<div class='discourse-lottery' #{attrs}></div>"
+module DiscourseLotteryV6
+  module DiscourseMarkdown
+    def self.setup
+      Discourse::Markdown.register_bbcode("lottery", "div") do |bbcode|
+        attrs =
+          %w[
+            name
+            draw_at
+            winner_count
+            min_participants
+            status
+            backup_strategy
+            specified_winners
+            prize_image_url
+            notes
+          ].map { |key| "data-#{key.dasherize}='#{bbcode.attrs[key]}'" if bbcode.attrs[key] }
+            .compact
+            .join(" ")
+
+        "<div class='discourse-lottery' #{attrs}></div>"
+      end
+    end
+  end
 end
