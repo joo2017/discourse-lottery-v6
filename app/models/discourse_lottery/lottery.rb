42d052feeeb3a1edd4a3a283303a1a714358743e
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-09-08 13:40:47 +0900

    Create lottery.rb

---
+++ app/models/discourse_lottery/lottery.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module DiscourseLottery
+  class Lottery < ActiveRecord::Base
+    self.table_name = "discourse_lottery_lotteries"
+
+    belongs_to :post, class_name: "Post"
+    has_many :participants,
+             class_name: "DiscourseLottery::Participant",
+             foreign_key: :lottery_id, # 明确指定外键
+             dependent: :destroy
+
+    enum status: { running: 0, finished: 1, cancelled: 2 }
+    enum draw_type: { random: 0, specified: 1 }
+    enum backup_strategy: { proceed: 0, cancel: 1 }
+
+    validates :draw_at, presence: true
+    validates :winner_count, presence: true, numericality: { greater_than: 0 }
+    validates :min_participants,
+              presence: true,
+              numericality: {
+                greater_than_or_equal_to: 0,
+              }
+
+    def topic
+      post&.topic
+    end
+  end
+end
