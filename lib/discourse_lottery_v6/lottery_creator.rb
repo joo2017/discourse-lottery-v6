6446bf341324730e040025e282e20d358e07a973
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-09-08 13:03:26 +0900

    Update lottery_creator.rb

--- lib/discourse_lottery_v6/lottery_creator.rb
+++ lib/discourse_lottery_v6/lottery_creator.rb
@@ -8,7 +8,6 @@ module DiscourseLotteryV6
     end
 
     def create_or_update
-      # 如果帖子里没有抽奖BBCode，但数据库里有关联的抽奖，就删除它
       if @lotteries_data.empty?
         @post.lottery&.destroy
         return
@@ -17,7 +16,6 @@ module DiscourseLotteryV6
       lottery_data = @lotteries_data.first
       lottery = @post.lottery || @post.build_lottery
 
-      # 智能判断抽奖方式
       if lottery_data[:specified_winners].present?
         lottery.draw_type = :specified
         winner_floors = lottery_data[:specified_winners].split(",").map(&:to_i).reject(&:zero?)
@@ -29,37 +27,33 @@ module DiscourseLotteryV6
         lottery.specified_winners = []
       end
 
-      # 填充其他属性
-      lottery.draw_at = Time.zone.parse(lottery_data[:draw_at])
+      lottery.draw_at = Time.zone.parse(lottery_data[:draw_at]) rescue nil
       lottery.min_participants = lottery_data[:min_participants].to_i
       lottery.backup_strategy = lottery_data[:backup_strategy] || :proceed
       lottery.name = lottery_data[:name]
       lottery.description = lottery_data[:description]
       lottery.prize_image_url = lottery_data[:prize_image_url]
       lottery.notes = lottery_data[:notes]
-      lottery.status = :running # 每次编辑都重置为running状态
+      lottery.status = :running
 
       if lottery.save
         schedule_jobs(lottery)
       else
-        # 如果保存失败，可以在这里处理错误，比如记录日志
-        Rails.logger.error("Failed to save lottery for post #{@post.id}: #{lottery.errors.full_messages.join(", ")}")
+        Rails.logger.error("[DiscourseLotteryV6] Lottery save failed for post #{@post.id}: #{lottery.errors.full_messages.join(", ")}")
       end
     end
 
     private
 
     def schedule_jobs(lottery)
-      # 调度开奖任务
-      Jobs.cancel_scheduled_job(:execute_lottery_draw, lottery_id: lottery.id)
-      Jobs.enqueue_at(lottery.draw_at, :execute_lottery_draw, lottery_id: lottery.id)
-
-      # 调度锁定帖子任务
-      lock_delay = SiteSetting.lottery_post_lock_delay_minutes
-      if lock_delay > 0
-        Jobs.cancel_scheduled_job(:lock_lottery_post, post_id: @post.id)
-        Jobs.enqueue_in(lock_delay.minutes, :lock_lottery_post, post_id: @post.id)
-      end
+      # Jobs.cancel_scheduled_job(:execute_lottery_draw, lottery_id: lottery.id)
+      # Jobs.enqueue_at(lottery.draw_at, :execute_lottery_draw, lottery_id: lottery.id)
+      #
+      # lock_delay = SiteSetting.lottery_post_lock_delay_minutes
+      # if lock_delay > 0
+      #   Jobs.cancel_scheduled_job(:lock_lottery_post, post_id: @post.id)
+      #   Jobs.enqueue_in(lock_delay.minutes, :lock_lottery_post, post_id: @post.id)
+      # end
     end
   end
 end
