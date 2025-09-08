4dc8cd60bbc1667d9f90d0ec3a2ebca6cdd4ff2d
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-09-08 13:12:14 +0900

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -17,50 +17,59 @@ end
 require_relative "lib/discourse_lottery_v6/engine"
 
 after_initialize do
-  # 加载所有库文件
+  # --- 所有插件逻辑都从这里开始 ---
+
+  # 1. 加载所有依赖文件
   require_relative "lib/discourse_markdown/discourse_lottery"
   require_relative "lib/discourse_lottery_v6/lottery_parser"
   require_relative "lib/discourse_lottery_v6/lottery_validator"
   require_relative "lib/discourse_lottery_v6/lottery_creator"
   require_relative "lib/discourse_lottery_v6/post_extension"
 
-  # 将我们的扩展应用到 Post 模型
+  # 2. 设置 BBCode 解析规则
+  DiscourseLotteryV6::DiscourseMarkdown.setup
+
+  # 3. 允许我们的 div 和 data-* 属性通过 HTML sanitizer
+  customize_html_whitelist do |whitelist|
+    whitelist[:attributes]["div"].concat(
+      %w[
+        data-name
+        data-draw-at
+        data-winner-count
+        data-min-participants
+        data-status
+        data-backup-strategy
+        data-specified-winners
+        data-prize-image-url
+        data-notes
+      ],
+    )
+  end
+
+  # 4. 将我们的扩展应用到 Post 模型
   reloadable_patch { Post.prepend(DiscourseLotteryV6::PostExtension) }
 
-  # 将 Lottery 数据添加到 Post 序列化器中
+  # 5. 将 Lottery 数据添加到 Post 序列化器中
   add_to_serializer(
     :post,
     :lottery,
     include_condition: -> { SiteSetting.lottery_enabled? && object.is_first_post? && object.lottery.present? },
   ) do
+    # 确保序列化器类存在
+    require_relative "app/serializers/discourse_lottery_v6/lottery_serializer"
     DiscourseLotteryV6::LotterySerializer.new(object.lottery, scope: scope, root: false).as_json
   end
 
-  # 监听帖子创建和编辑事件
+  # 6. 监听帖子创建和编辑事件
   on(:post_created) do |post, opts, user|
-    if post.is_first_post?
+    if post.is_first_post? && SiteSetting.lottery_enabled?
       DiscourseLotteryV6::LotteryCreator.new(post).create_or_update
     end
   end
 
   on(:post_edited) do |post, topic_changed, post_revision|
-    if post.is_first_post?
+    if post.is_first_post? && SiteSetting.lottery_enabled?
       DiscourseLotteryV6::LotteryCreator.new(post).create_or_update
     end
-  end
-  
-  # 允许我们的div和data-*属性通过HTML sanitizer
-  customize_html_whitelist do |whitelist|
-    whitelist[:attributes]["div"].concat(%w[
-      data-name
-      data-draw_at
-      data-winner_count
-      data-min_participants
-      data-status
-      data-backup_strategy
-      data-specified_winners
-      data-prize_image_url
-      data-notes
-    ])
   end
 end
